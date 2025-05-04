93849aab8d5332f0d2d3c50c7abf60eee28d78ff
Enzonly <71802637+enzw@users.noreply.github.com>
2025-05-04 19:53:31 +0700

    add some const for capture picture function

--- scriptPhotobooth.js
+++ scriptPhotobooth.js
@@ -7,3 +7,9 @@ navigator.mediaDevices.getUserMedia({ video: true })
             .catch(function(error) {
                 console.error("Error accessing the camera: ", error);
             });
+
+const video = document.getElementById('videoElement');
+const canvas = document.getElementById('canvas');
+const countdown = document.getElementById('countdown');
+const context = canvas.getContext('2d');
+
