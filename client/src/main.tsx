102043b789d82b5775549ace32f33de31d99f64d
enzo <71802637+enzw@users.noreply.github.com>
2026-09-01 09:36:45 +0700

    Remove OpenSearchServerless CreateIndex and DeleteIndex bindings and their associated files
    - Deleted CreateIndex and DeleteIndex TypeScript definitions and their corresponding map files.
    - Removed JavaScript implementations for CreateIndex and CreateIndexHttp.
    - Cleaned up related source maps for the removed files.
    - This change simplifies the OpenSearchServerless module by removing unused bindings.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@/components/theme-provider'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      enableColorScheme
+      disableTransitionOnChange
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
